e92adc183730b83a4301786f5ee36eb3e167e20b
masterstoney <tgmsstoney@gmail.com>
2019-02-25 08:20:19 -0600

    Initial Commit

---
+++ SOARGuide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SOARGuide
+//
+//  Created by Tendaishe Gwini on 25/02/2019.
+//  Copyright © 2019 Tendaishe Gwini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
